c33900f91070a1e646a53d92d3c66e878405638f
Velko Nikolov <velko.nikolov@tick42.com>
2020-08-08 17:59:59 +0300

    deploy

--- bot.js
+++ bot.js
@@ -46,62 +46,52 @@ class EchoBot extends ActivityHandler {
                 await context.sendActivity(MessageFactory.text("Good human", "Good human"));
             } else if (text.match(/version/i)) {
                 await context.sendActivity(MessageFactory.text(pkg.version, pkg.version));
+            } else if (text.match(/\b(hi|hello)\b/i)) {
+                await context.sendActivity(MessageFactory.text("Well hello there.", "Well hello there."));
+            } else if (text.match(/who's (the best|the greatest|right)/i)) {
+                await context.sendActivity(MessageFactory.text("Why, Velko, of course.", "Why, Velko, of course."));
             } else {
-                    // todo: support youtu.be
-                    const rx = "https?:[/][/](www[.])?youtube[.]com[/]watch[?](.*&)?v=([a-zA-Z0-9_-]+)";
-                    //await context.sendActivity(MessageFactory.text("here", "here"));
-                    const urls = text.match(new RegExp(rx, "ig")) || text.match(/https?:[/][/]youtu[.]be[/]([a-zA-Z0-9_-]+)/ig);
-                    //console.log(JSON.stringify(urls));
-                    // await context.sendActivity(MessageFactory.text(JSON.stringify(urls), JSON.stringify(urls)));
-                    if (urls && urls.length) {
-                        for (const url_ of urls) {
-                            const url = url_.replace(/https?:[/][/]youtu[.]be[/]/i, "https://www.youtube.com/watch?v=");
-                            //await context.sendActivity(MessageFactory.text(url, url));
-                            //console.log(url);
-                            let reply = "";
-                            await new Promise((rs) => {
-                                var callback = function(res) {
-                                  //console.log(`STATUS: ${res.statusCode}`);
-                                  //console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-                                  res.setEncoding('utf8');
-                                  res.on('data', function (chunk) {
-                                    reply += chunk;
-                                    //console.log("chunk" + chunk)
-                                  });
+                const rx = "https?:[/][/](www[.])?youtube[.]com[/]watch[?](.*&)?v=([a-zA-Z0-9_-]+)";
+                const urls = text.match(new RegExp(rx, "ig")) || text.match(/https?:[/][/]youtu[.]be[/]([a-zA-Z0-9_-]+)/ig);
+                if (urls && urls.length) {
+                    for (const url_ of urls) {
+                        const url = url_.replace(/https?:[/][/]youtu[.]be[/]/i, "https://www.youtube.com/watch?v=");
+                        let reply = "";
+                        await new Promise((rs) => {
+                            var callback = function(res) {
+                              //console.log(`STATUS: ${res.statusCode}`);
+                              //console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+                              res.setEncoding('utf8');
+                              res.on('data', function (chunk) {
+                                reply += chunk;
+                              });
 
-                                  res.on('end', function () {
-                                    //console.log('end');
-                                    rs();
-                                  });
-                                };
+                              res.on('end', function () {
+                                rs();
+                              });
+                            };
 
-                                const options = {
-                                    host: "www.youtube.com",
-                                    path: url.replace(/.*[.]com[/]/i, "/"),
-                                    headers: {
-                                      accept: "*/*",
-                                      ["user-agent"]: "curl"
-                                    }
-                                };
-                                //console.log(options);
-                                http.get(options, callback).end();
-                            });
-                            const matched = reply.match(/<title>([^<]+)<[/]title>/);
-                            //console.log(reply);
-                            const replyText = (matched && matched.length && matched[1]) ?
-                                url_ + ": " + matched[1].replace(" - YouTube", "") :
-                                url_ + ": <can't find title>";
-                            await context.sendActivity(MessageFactory.text(replyText, replyText));
-                            break;
-                        }
-                    } else if (text.match(/\b(hi|hello)\b/i)) {
-                        await context.sendActivity(MessageFactory.text("Well hello there.", "Well hello there."));
-                    } else if (text.match(/who's (the best|the greatest|right)/i)) {
-                        await context.sendActivity(MessageFactory.text("Why, Velko, of course.", "Why, Velko, of course."));
-                    } else {
-                        await context.sendActivity(MessageFactory.text("What? Make sense, you creature.", "What? Make sense, you creature."));
+                            const options = {
+                                host: "www.youtube.com",
+                                path: url.replace(/.*[.]com[/]/i, "/"),
+                                headers: {
+                                  accept: "*/*",
+                                  ["user-agent"]: "curl"
+                                }
+                            };
+                            http.get(options, callback).end();
+                        });
+                        const matched = reply.match(/<title>([^<]+)<[/]title>/);
+                        const replyText = (matched && matched.length && matched[1]) ?
+                            url_ + ": " + matched[1].replace(" - YouTube", "") :
+                            url_ + ": <can't find title>";
+                        await context.sendActivity(MessageFactory.text(replyText, replyText));
+                        break;
                     }
-                } 
+                } else {
+                    await context.sendActivity(MessageFactory.text("What? Make sense, you creature.", "What? Make sense, you creature."));
+                }
+            }
             }
             catch (err) {
                 await context.sendActivity(MessageFactory.text("Error: " + err.message, "Error: " + err.message));
