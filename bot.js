8e404759405d11ac2a1e89c2173c5636900df27e
Velko Nikolov <velko.nikolov@tick42.com>
2020-08-08 17:43:05 +0300

    deploy

--- bot.js
+++ bot.js
@@ -38,7 +38,7 @@ class EchoBot extends ActivityHandler {
                 await context.sendActivity(MessageFactory.text("Ha. Ha. Ha.", "Ha. Ha. Ha."));
             } else if (text.match(/^raw /) {
                 const str = safeStringify(context);
-                await context.sendActivity(MessageFactory.text(context, context));
+                await context.sendActivity(MessageFactory.text(str, str));
             } else if (text.match(/\b(fuck|ass|shit|stupid|moron|dumb|cunt|fool)/i)) {
                 await context.sendActivity(MessageFactory.text("Bite my shiny metal ass!", "Bite my shiny metal ass!"));
             } else if (text.match(/good bot/i)) {
