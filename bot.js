e8030e9962d872ac45ad221f497df7d18f8276ac
Velko Nikolov <velko.nikolov@tick42.com>
2021-07-21 16:56:39 +0300

    deploy

--- bot.js
+++ bot.js
@@ -334,6 +334,7 @@ eval ["  /\\ ___ /\\", " (  o   o  )", "  \\  >#<  /", "  /       \\", " /      
             } else if (text.match(/\bget-title /i)) {
                 let reply = "";
                 const url = (text.match(/https?.*/) || [])[0];
+                await context.sendActivity(MessageFactory.text(url, url));
                 await context.sendActivity(MessageFactory.text(getHost(url) + " " + getPath(url), getHost(url) + " " + getPath(url)));
                 await new Promise((rs) => {
                     var callback = function(res) {
